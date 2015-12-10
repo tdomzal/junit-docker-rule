65a403b29fd420a83159dc2bd4fff8389cc06f37
Tomasz Domzal <tdomzal@gmail.com>
2015-12-10 20:38:15 +0100

    cleanup DockerRuleClassRuleInSuperclassTests

--- src/test/java/pl/domzal/junit/docker/rule/DockerRuleClassRuleInSuperclassTests.java
+++ src/test/java/pl/domzal/junit/docker/rule/DockerRuleClassRuleInSuperclassTests.java
@@ -1,14 +1,9 @@
 package pl.domzal.junit.docker.rule;
 
-import static org.hamcrest.CoreMatchers.containsString;
-import static org.junit.Assert.assertThat;
-
 import java.util.concurrent.TimeoutException;
 
 import org.junit.ClassRule;
 import org.junit.Test;
-
-import pl.domzal.junit.docker.rule.DockerRule;
 
 public class DockerRuleClassRuleInSuperclassTests {
 
@@ -19,27 +14,15 @@ public class DockerRuleClassRuleInSuperclassTests {
             .waitForMessage("12345678")
             .build();
 
-    public static Ble a = new Ble();
-
-    public static class Ble {
-        public Ble() {
-            System.out.println("------------- BLE -------------");
-        }
-    }
-
     public static class TestCase1 extends DockerRuleClassRuleInSuperclassTests {
         @Test
-        public void shouldEcho1() throws TimeoutException, InterruptedException {
-            testee.waitFor("12345678", 10);
-            assertThat(testee.getLog(), containsString("12345678"));
+        public void shouldRun1() throws TimeoutException, InterruptedException {
         }
     }
 
     public static class TestCase2 extends DockerRuleClassRuleInSuperclassTests {
         @Test
-        public void shouldEcho2() throws TimeoutException, InterruptedException {
-            testee.waitFor("12345678", 10);
-            assertThat(testee.getLog(), containsString("12345678"));
+        public void shouldRun2() throws TimeoutException, InterruptedException {
         }
     }
 }
