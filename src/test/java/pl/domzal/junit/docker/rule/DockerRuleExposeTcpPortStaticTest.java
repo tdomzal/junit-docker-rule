be7722f5d97e755916fb783b31347d56d0329913
Tomasz Domzal <tdomzal@gmail.com>
2016-02-05 17:25:24 +0100

    (debugging travis ci test cases failing) log network config

---
+++ src/test/java/pl/domzal/junit/docker/rule/DockerRuleExposeTcpPortStaticTest.java
@@ -0,0 +1,49 @@
+package pl.domzal.junit.docker.rule;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.spotify.docker.client.DockerException;
+import com.spotify.docker.client.messages.ContainerInfo;
+import com.spotify.docker.client.messages.NetworkSettings;
+
+public class DockerRuleExposeTcpPortStaticTest {
+
+    private static final Logger log = LoggerFactory.getLogger(DockerRuleExposeTcpPortStaticTest.class);
+
+    @Rule
+    public DockerRule testee = DockerRule.builder() //
+            .imageName("alpine") //
+            .expose("4444", "4444") //
+            .cmd("sh", "-c", "echo started; nc -l -p 4444") //
+            .waitForMessage("started") //
+            .build();
+
+    @Before
+    public void logNetworkConfig() throws DockerException, InterruptedException {
+        ContainerInfo containerInfo = testee.getDockerClient().inspectContainer(testee.getContainerId());
+        NetworkSettings networkSettings = containerInfo.networkSettings();
+        //networkSettings.
+        log.debug("containerInfo.network: {}", networkSettings);
+    }
+
+    @Test
+    public void shouldExposeSpecifiedPort() throws Throwable {
+        DockerRule sender = DockerRule.builder() //
+                .imageName("alpine") //
+                .extraHosts("serv:"+testee.getDockerContainerGateway())
+                .cmd("sh", "-c", "echo 12345 | nc serv 4444; echo done") //
+                .waitForMessage("done") //
+                .build();
+        sender.before();
+        try {
+            testee.waitFor("12345", 5);
+        } finally {
+            sender.after();
+        }
+    }
+
+}
